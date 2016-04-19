31550422da6143ead378c3ed559abc51b21b279f
450287475 <450287475@qq.com>
2016-04-20 01:01:17 +0800

    实现了背单词的基本功能,目前只有四级背单词功能

--- app/src/main/java/com/wangdao/mutilword/activity/HomeActivity.java
+++ app/src/main/java/com/wangdao/mutilword/activity/HomeActivity.java
@@ -1,14 +1,9 @@
 package com.wangdao.mutilword.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.wangdao.mutilword.R;
-
-import  View.ExplosionField;
 
 public class HomeActivity extends Activity {
 
@@ -17,10 +12,10 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         //设置监听点击的图标
-        ExplosionField explosionField = new ExplosionField(HomeActivity.this);
+       /* ExplosionField explosionField = new ExplosionField(HomeActivity.this);
 
         explosionField.addListener(findViewById(R.id.ll_root));
-
+*/
     }
 }
 
