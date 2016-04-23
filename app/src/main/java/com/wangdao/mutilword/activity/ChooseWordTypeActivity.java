0caf9ef3f2a87fa29364925475922d3ade8d49b8
450287475 <450287475@qq.com>
2016-04-23 23:40:12 +0800

    更新了选择单词本和背单词界面的UI

--- app/src/main/java/com/wangdao/mutilword/activity/ChooseWordTypeActivity.java
+++ app/src/main/java/com/wangdao/mutilword/activity/ChooseWordTypeActivity.java
@@ -3,8 +3,12 @@ package com.wangdao.mutilword.activity;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -20,7 +24,6 @@ import com.wangdao.mutilword.constant.Constant;
 import com.wangdao.mutilword.dao.RepeatWordDao;
 import com.wangdao.mutilword.dialog.Effectstype;
 import com.wangdao.mutilword.dialog.NiftyDialogBuilder;
-import com.wangdao.mutilword.utils.ListViewSwipeGesture;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -68,8 +71,19 @@ public class ChooseWordTypeActivity extends Activity {
         rb_dialog_cet6 = (RadioButton) view.findViewById(R.id.rb_dialog_cet6);
         rb_dialog_master = (RadioButton) view.findViewById(R.id.rb_dialog_master);
 
-        //初始化要背单词的列表
-        lv_chooseWordType_wordlist = (ListView) findViewById(R.id.lv_chooseWordType_wordlist);
+
+        //改变状态栏颜色
+        Window window = this.getWindow();
+        //设置透明状态栏,这样才能让 ContentView 向上
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //设置状态栏颜色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(Color.RED);
+
+        }
+
     }
 
     private void initData() {
@@ -79,43 +93,9 @@ public class ChooseWordTypeActivity extends Activity {
         daoRepeatWord = new RepeatWordDao(this, "oldWord.db", 1);
         word_infos = daoRepeatWord.getWord();
         repeatWordListAdapter = new RepeatWordListAdapter(this, word_infos);
-        lv_chooseWordType_wordlist.setAdapter(repeatWordListAdapter);
-        ListViewSwipeGesture listViewSwipeGesture =
-                new ListViewSwipeGesture(lv_chooseWordType_wordlist, swipeListener, this);
-        listViewSwipeGesture.SwipeType = ListViewSwipeGesture.Double;
-        lv_chooseWordType_wordlist.setOnTouchListener(listViewSwipeGesture);
+
     }
 
-
-    ListViewSwipeGesture.TouchCallbacks swipeListener = new ListViewSwipeGesture.TouchCallbacks() {
-        @Override
-        public void FullSwipeListView(int position) {
-            Toast.makeText(ChooseWordTypeActivity.this, "Action_2", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void HalfSwipeListView(int position) {
-            Word_info word_info = word_infos.remove(position);
-            repeatWordListAdapter.notifyDataSetChanged();
-            daoRepeatWord.update(word_info.getId(), 0, -1);
-            Toast.makeText(ChooseWordTypeActivity.this, "删除成功", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void OnClickListView(int position) {
-
-        }
-
-        @Override
-        public void LoadDataForScroll(int count) {
-
-        }
-
-        @Override
-        public void onDismiss(ListView listView, int[] reverseSortedPositions) {
-
-        }
-    };
 
     @Override
     public void onBackPressed() {
@@ -136,12 +116,12 @@ public class ChooseWordTypeActivity extends Activity {
         //选择单词页
         niftyDialogBuilder = dialogBuilder
                 .withTitle("选择要背的单词本")                                  //.withTitle(null)  no title
-                .withTitleColor("#FFFFFF")                                  //def
+                .withTitleColor("#bfcce6")                                  //def
                 .withDividerColor("#11000000")                              //def
                 .withMessage("请问你想复习哪本书的单词?")                     //.withMessage(null)  no Msg
-                .withMessageColor("#FFFFFF")                                //def
+                .withMessageColor("#bfcce6")                                //def
                 .withIcon(getResources().getDrawable(R.drawable.icon))
-                .isCancelableOnTouchOutside(true)                           //def    | isCancelable(true)
+                .isCancelableOnTouchOutside(false)                           //def    | isCancelable(true)
                 .withDuration(700)                                          //def
                 .withEffect(effect)                                         //def Effectstype.Slidetop
                 .withButton1Text("OK")                                      //def gone
@@ -221,7 +201,7 @@ public class ChooseWordTypeActivity extends Activity {
         BmobQuery<Bmob_word_info> query = new BmobQuery<>();
         query.addWhereEqualTo("username", ApplicationInfo.userInfo.getUsername());
         query.setLimit(Integer.MAX_VALUE);
-        query.findObjects(this,  new FindListener<Bmob_word_info>() {
+        query.findObjects(this, new FindListener<Bmob_word_info>() {
 
             public ArrayList<BmobObject> update_word_infos;
             public ArrayList<Word_info> word_infos_db;
@@ -230,36 +210,36 @@ public class ChooseWordTypeActivity extends Activity {
             public void onSuccess(List<Bmob_word_info> list) {
                 //弹出进度框,避免用户误操作
 
-                Toast.makeText(ChooseWordTypeActivity.this, "服务器端共有"+list.size()+"条数据,正在同步到本地数据库", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ChooseWordTypeActivity.this, "服务器端共有" + list.size() + "条数据,正在同步到本地数据库", Toast.LENGTH_SHORT).show();
 
                 //获得所有背过单词的集合,该集合通过运算剩余的所有元素都是服务器没有的,用insert方法添加到服务器中
                 word_infos_db = daoRepeatWord.getWord();
 
                 //progressInfo保存总进度,当前进度,同步单词失败的个数
                 final ProgressInfo progressInfo = new ProgressInfo();
-                progressInfo.total=list.size();//总进度
-                progressInfo. current=0;//当前进度
-                progressInfo.failure=0;//同步单词失败的个数
+                progressInfo.total = list.size();//总进度
+                progressInfo.current = 0;//当前进度
+                progressInfo.failure = 0;//同步单词失败的个数
                 progressDialog.setMax(progressInfo.total);
 
                 //遍历从云端获得的单词.若和本地数据库中的单词相同,则将本地数据库中的该单词用update的方法更新到云端
-                for (final Bmob_word_info bmob_word_info:list){
+                for (final Bmob_word_info bmob_word_info : list) {
                     //查询该单词是否存在本地数据库,存在返回本地数据库的单词信息,否则返回null
                     Word_info exist = daoRepeatWord.isExist(bmob_word_info.getWord());
 
-                    if (exist!=null){
+                    if (exist != null) {
                         //单词在本地数据库存在,则从word_infos_db去掉,
-                            word_infos_db.remove(exist);
+                        word_infos_db.remove(exist);
 
                         //如果从云端获得的单词的最后背诵日期和存在本地单词的最后背诵日期相同,则没有同步的必要.
-                        if (exist.getDate()==bmob_word_info.getDate()){
+                        if (exist.getDate() == bmob_word_info.getDate()) {
                             //更新进度条
                             refreshProgressDialog(progressInfo);
-                            System.out.println("本地数据库和服务器端数据相同"+exist.getWord());
+                            System.out.println("本地数据库和服务器端数据相同" + exist.getWord());
                             progressInfo.total--;
                             progressInfo.current--;
                             continue;
-                        }else {
+                        } else {
                             //更新该单词
                             bmob_word_info.setDate(exist.getDate());
                             bmob_word_info.update(ChooseWordTypeActivity.this, bmob_word_info.getObjectId(), new UpdateListener() {
@@ -267,7 +247,7 @@ public class ChooseWordTypeActivity extends Activity {
                                 public void onSuccess() {
                                     //更新进度条
                                     refreshProgressDialog(progressInfo);
-                                    System.out.println("更新成功"+bmob_word_info.getWord());
+                                    System.out.println("更新成功" + bmob_word_info.getWord());
                                 }
 
                                 @Override
@@ -275,24 +255,24 @@ public class ChooseWordTypeActivity extends Activity {
                                     refreshProgressDialog(progressInfo);
                                     //若更新失败,则failure++;
                                     progressInfo.failure++;
-                                    System.out.println("更新成功"+bmob_word_info.getWord());
-                                    System.out.println(bmob_word_info.getWord()+"更新失败:"+s);
+                                    System.out.println("更新成功" + bmob_word_info.getWord());
+                                    System.out.println(bmob_word_info.getWord() + "更新失败:" + s);
                                 }
                             });
                         }
 
-                    }else {//单词在本地数据库不存在,则加入本地数据库
+                    } else {//单词在本地数据库不存在,则加入本地数据库
                         refreshProgressDialog(progressInfo);
-                        daoRepeatWord.insert(new Word_info(bmob_word_info.getWord(),bmob_word_info.getTrans(),
-                                bmob_word_info.getPhonetic(),bmob_word_info.getTags(),bmob_word_info.getRepeat(),-1,bmob_word_info.getDate()));
+                        daoRepeatWord.insert(new Word_info(bmob_word_info.getWord(), bmob_word_info.getTrans(),
+                                bmob_word_info.getPhonetic(), bmob_word_info.getTags(), bmob_word_info.getRepeat(), -1, bmob_word_info.getDate()));
                     }
                 }
                 //更新进度条的最大进度
-                progressInfo.total+=word_infos_db.size();
+                progressInfo.total += word_infos_db.size();
                 progressDialog.setMax(progressInfo.total);
 
                 //遍历所有还存在word_infos_db集合中的元素,这些元素代表着云端不存在而本地数据库存在的单词.将这些单词用insert方法加入到云端
-                for(Word_info word_info:word_infos_db){
+                for (Word_info word_info : word_infos_db) {
                     final Bmob_word_info bmob_word_info = new Bmob_word_info(word_info.getWord(), word_info.getTrans(), word_info.getPhonetic(),
                             word_info.getTags(), word_info.getRepeat(), word_info.getDate(), ApplicationInfo.userInfo.getUsername());
 
@@ -300,14 +280,14 @@ public class ChooseWordTypeActivity extends Activity {
                         @Override
                         public void onSuccess() {
                             refreshProgressDialog(progressInfo);
-                            System.out.println(bmob_word_info.getWord()+"插入成功");
+                            System.out.println(bmob_word_info.getWord() + "插入成功");
                         }
 
                         @Override
                         public void onFailure(int i, String s) {
                             refreshProgressDialog(progressInfo);
                             progressInfo.failure++;
-                            System.out.println(bmob_word_info.getWord()+"插入失败"+s);
+                            System.out.println(bmob_word_info.getWord() + "插入失败" + s);
                         }
                     });
 
@@ -319,10 +299,10 @@ public class ChooseWordTypeActivity extends Activity {
             private void refreshProgressDialog(ProgressInfo progressInfo) {
                 progressInfo.current++;
                 progressDialog.setProgress(progressInfo.current);
-                if(progressInfo.current==progressInfo.total){
-                    if(progressInfo.failure!=0){
-                        Toast.makeText(ChooseWordTypeActivity.this,"同步完成,还有"+progressInfo.failure+"个单词没同步成功...下次可一起同步",Toast.LENGTH_SHORT);
-                    }else {
+                if (progressInfo.current == progressInfo.total) {
+                    if (progressInfo.failure != 0) {
+                        Toast.makeText(ChooseWordTypeActivity.this, "同步完成,还有" + progressInfo.failure + "个单词没同步成功...下次可一起同步", Toast.LENGTH_SHORT);
+                    } else {
                         Toast.makeText(ChooseWordTypeActivity.this, "完全同步成功!", Toast.LENGTH_SHORT).show();
                     }
                     progressDialog.dismiss();
@@ -340,4 +320,8 @@ public class ChooseWordTypeActivity extends Activity {
 
     }
 
+    //跳转到单词本页面
+    public void goToWordBook(View view) {
+        startActivity(new Intent(this,WordBookActivity.class));
+    }
 }
